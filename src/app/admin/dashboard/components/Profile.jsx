cac212a976023a40f76ae0dbf8aa412d3b08d0c5
Tarun Gehlaut <tarungerhlaut330@gmail.com>
2024-01-17 20:14:14 +0530

    initial desing v1 - removed unneccessory files

--- src/app/admin/dashboard/components/Profile.jsx
+++
@@ -1,8 +0,0 @@
-// Profile component
-const Profile = () => (
-  <div className="hidden md:block text-sm md:text-md text-black dark:text-white">
-    John Doe
-  </div>
-);
-
-export default Profile;
